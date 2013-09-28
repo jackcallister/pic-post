70d8367941bbf14f94be5a6b1c762192bb96597f
Jack Callister <jarsbe@gmail.com>
2013-09-28 14:19:16 +1200

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PicPost::Application.config.secret_key_base = '23fcfe484372a251e57085016b16b2f87b8486e314d65ef198bd648c6f617bfd5d7c0a090e083907e173c67ff072882aef990be350d2613cc736118fe5849096'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PicPost::Application.config.session_store :cookie_store, key: '_picPost_session'
